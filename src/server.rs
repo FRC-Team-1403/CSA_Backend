fe3d899ccdd6d9855663f8ada1fa92ad86c8f247
SpaceT0 <space@spaceturtle.tech>
2023-02-08 15:33:27 -0500

    Sentry Fixes

--- src/server.rs
+++ src/server.rs
@@ -8,8 +8,15 @@ pub async fn run() {
     let join = tokio::spawn(async move {
         let mut year = YearData::new();
         loop {
+            let tx_ctx = sentry::TransactionContext::new(
+                "Updating new Year Value",
+                "run() function",
+            );
+            let transaction = sentry::start_transaction(tx_ctx);
+
             info!("Updating year value: ");
             year = update(year).await;
+            transaction.finish();
             thread::sleep(Duration::from_secs(360))
         }
     });
@@ -19,6 +26,12 @@ pub async fn run() {
 async fn update(mut year: YearData) -> YearData {
     let mut error: u8 = 0;
     loop {
+        let tx_ctx = sentry::TransactionContext::new(
+            "Update Year",
+            "Running from update()",
+        );
+        let transaction = sentry::start_transaction(tx_ctx);
+
         match year.update(2022).await {
             Ok(e) => {
                 return e;
@@ -28,6 +41,7 @@ async fn update(mut year: YearData) -> YearData {
                 error += 1;
             }
         }
+        transaction.finish();
         if error > 120 {
             warn!("critical Failure, skipping");
             return year;
