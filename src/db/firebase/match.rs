f5e75681cfeb9d4e237681a4b8e090b5051cd875
carghai <carghai74@gmail.com>
2023-02-20 13:39:55 -0500

    more speed up with threads

--- src/db/firebase/match.rs
+++ src/db/firebase/match.rs
@@ -1,6 +1,7 @@
 use crate::comp::event::math::EventData;
 use crate::ram::ENV;
 use log::warn;
+use rayon::prelude::*;
 use std::io;
 use std::process::Command;
 
@@ -13,27 +14,30 @@ impl MatchStore {
         Self { data }
     }
     pub fn send(self) -> Result<(), io::Error> {
-        for raw_json in self.data {
-            let json = serde_json::to_string(&raw_json)?;
-            let firestore_location = &ENV.firestore_collection;
-            let result = Command::new("microService/firestore_send/bin")
-                .args([
-                    json,
-                    firestore_location.to_owned(),
-                    raw_json.team.to_string(),
-                    "Matches".to_owned(),
-                    raw_json.match_number.to_string(),
-                ])
-                .output()?;
-            let uft8_output = String::from_utf8(result.clone().stdout).unwrap_or(String::new());
-            if uft8_output.is_empty() {
-                warn!(
-                    "{}",
-                    String::from_utf8(result.clone().stderr).unwrap_or("Utf8 error".to_owned())
-                );
-            }
-            println!("{uft8_output}",);
-        }
+        self.data
+            .par_iter()
+            .try_for_each(|raw_json| -> Result<(), io::Error> {
+                let json = serde_json::to_string(&raw_json)?;
+                let firestore_location = &ENV.firestore_collection;
+                let result = Command::new("microService/firestore_send/bin")
+                    .args([
+                        json,
+                        firestore_location.to_owned(),
+                        raw_json.team.to_string(),
+                        "Matches".to_owned(),
+                        raw_json.match_number.to_string(),
+                    ])
+                    .output()?;
+                let uft8_output = String::from_utf8(result.clone().stdout).unwrap_or(String::new());
+                if uft8_output.is_empty() {
+                    warn!(
+                        "{}",
+                        String::from_utf8(result.stderr).unwrap_or("Utf8 error".to_owned())
+                    );
+                }
+                println!("{uft8_output}",);
+                Ok(())
+            })?;
         Ok(())
     }
 }
