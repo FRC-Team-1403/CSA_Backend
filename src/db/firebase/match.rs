2f26783755fbadbc25c065ff760e3afa2309c726
Carghai <carghai74@gmail.com>
2023-02-20 21:39:59 -0500

    update of cashe

--- src/db/firebase/match.rs
+++ src/db/firebase/match.rs
@@ -1,5 +1,5 @@
 use crate::comp::event::math::EventData;
-use crate::ram::ENV;
+use crate::ram::{CACHE_MATCH, ENV};
 use log::warn;
 use rayon::prelude::*;
 use std::process::Command;
@@ -19,28 +19,49 @@ impl MatchStore {
                 .data
                 .par_iter()
                 .try_for_each(|raw_json| -> Result<(), io::Error> {
-                    let json = serde_json::to_string(&raw_json)?;
-                    let firestore_location = &ENV.firestore_collection;
-                    let result = Command::new("microService/firestore_send/bin")
-                        .args([
-                            json,
-                            firestore_location.to_owned(),
-                            raw_json.team.to_string(),
-                            "Matches".to_owned(),
-                            raw_json.match_number.to_string(),
-                        ])
-                        .output()?;
-                    let uft8_output =
-                        String::from_utf8(result.clone().stdout).unwrap_or(String::new());
-                    if uft8_output.is_empty() {
-                        warn!(
-                            "{}",
-                            String::from_utf8(result.stderr).unwrap_or("Utf8 error".to_owned())
-                        );
+                    if check_cache(raw_json, &raw_json.team) {
+                        let json = serde_json::to_string(&raw_json)?;
+                        let firestore_location = &ENV.firestore_collection;
+                        let result = Command::new("microService/firestore_send/bin")
+                            .args([
+                                json,
+                                firestore_location.to_owned(),
+                                raw_json.team.to_string(),
+                                "Matches".to_owned(),
+                                raw_json.match_number.to_string(),
+                            ])
+                            .output()?;
+                        let uft8_output =
+                            String::from_utf8(result.clone().stdout).unwrap_or(String::new());
+                        if uft8_output.is_empty() {
+                            warn!(
+                                "{}",
+                                String::from_utf8(result.stderr).unwrap_or("Utf8 error".to_owned())
+                            );
+                        }
+                        println!("Sent for {} with output of {uft8_output}", raw_json.team);
                     }
-                    println!("Sent for {} with output of {uft8_output}", raw_json.team);
                     Ok(())
                 });
         });
     }
+}
+fn check_cache(year: &EventData, team_num: &u16) -> bool {
+    if let Ok(mut hash) = CACHE_MATCH.lock() {
+        let add;
+        if let Some(data) = hash.get(team_num) {
+            for check in data {
+                if check == year {
+                    return false;
+                }
+            }
+            let mut data = data.to_owned();
+            data.push(year.to_owned());
+            add = data
+        } else {
+            add = vec![year.to_owned()]
+        }
+        hash.insert(team_num.to_owned(), add);
+    }
+    true
 }
