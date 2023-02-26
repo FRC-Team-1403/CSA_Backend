3fd40a7bfc035f615a6edb3aa2d7729d02901d61
Carghai <carghai74@gmail.com>
2023-02-25 22:42:20 -0500

    shell script works

--- setup.sh
+++ setup.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 
+
+
 echo "Updating System"
 
 sudo pacman -Syyu
 
+brew upgrade
+
+sudo apt update
+
+sudo apt upgrade
+
 git pull
+
+# shellcheck disable=SC2164
+cd microService
+
+cd firestore_send
+
+go build
+
+# shellcheck disable=SC2103
+cd ..
+
+cd ..
 
 cargo clean
 
