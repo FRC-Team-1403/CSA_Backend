b11510daee7d35e24711ec1b47e1af4bbddee10f
Carghai <carghai74@gmail.com>
2023-03-21 13:16:43 -0400

    ai check data

--- src/comp/ai/train.rs
+++ src/comp/ai/train.rs
@@ -2,7 +2,7 @@ use std::{thread, time::Duration};
 
 use rayon::prelude::*;
 
-use crate::ram::ENV;
+use crate::{comp::shared::avg, ram::ENV};
 
 #[test]
 fn init() {
@@ -14,5 +14,12 @@ async fn train() {
     thread::sleep(Duration::from_secs(3));
     let api_data = crate::comp::http::get_match().await.unwrap();
     //data is recived, time to test
-    let train_results: Vec<f32> = vec![0; 10000].par_iter().filter_map(|_| None).collect();
+    let train_results: Vec<i16> = vec![0; 10000].par_iter().filter_map(|_| None).collect();
+    let avg = avg(train_results);
+    if avg < 90.0 {
+        panic!(
+            "Ai test failed with a score less then 90\n the ai score is: {}",
+            avg
+        )
+    }
 }
