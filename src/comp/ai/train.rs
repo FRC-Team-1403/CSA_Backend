35af1e7bbc8ef8c4e7d9ef264435b8021eb6fcda
Carghai <carghai74@gmail.com>
2023-03-21 12:38:58 -0400

    iter fix

---
+++ src/comp/ai/train.rs
@@ -0,0 +1,18 @@
+use std::{thread, time::Duration};
+
+use rayon::prelude::*;
+
+use crate::ram::ENV;
+
+#[test]
+fn init() {
+    dbg!(&ENV.teams);
+}
+
+#[tokio::test]
+async fn train() {
+    thread::sleep(Duration::from_secs(3));
+    let api_data = crate::comp::http::get_match().await.unwrap();
+    //data is recived, time to test
+    vec![0; 10000].par_iter().for_each(|_| {});
+}
