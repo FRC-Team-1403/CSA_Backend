32bd920903427e7b53277c0f802badedfbfa6eed
Carghai <carghai74@gmail.com>
2023-03-21 16:36:54 -0400

    training almost done

--- src/comp/ai/train.rs
+++ src/comp/ai/train.rs
@@ -2,7 +2,14 @@ use std::{thread, time::Duration};
 
 use rayon::prelude::*;
 
-use crate::{comp::shared::avg, ram::ENV};
+use crate::{
+    comp::{
+        ai::Ai,
+        avg::{math::YearAround, year_around_main::YearData},
+        shared::avg,
+    },
+    ram::ENV,
+};
 
 use rand::prelude::*;
 
@@ -26,22 +33,51 @@ async fn train() {
                     predict, train
                 );
             }
+            let train = YearAround::new(train.to_owned());
             let teams_br: Vec<(u16, f32)> = ENV
                 .teams
                 .par_iter()
                 .map(|team| {
-                    let cool = "yes";
-                    (team.to_owned(), 0.0)
+                    let ai_data = Ai::calc_match(&train, team);
+                    (team.to_owned(), ai_data)
                 })
                 .collect();
-            todo!()
+            let predict = predict.first().unwrap().to_owned();
+            let red = predict.alliances.red.team_keys;
+            let blue = predict.alliances.blue.team_keys;
+            let red_br = avg_br(red, &teams_br);
+            let blue_br = avg_br(blue, &teams_br);
+            drop(teams_br);
+            let winner = predict.winning_alliance;
+            let winner_ai = {
+                if red_br < blue_br {
+                    "blue"
+                } else {
+                    "red"
+                }
+            };
+            todo!();
         })
         .collect();
     let avg = avg(train_results);
-    if avg < 90.0 {
+    if avg < 0.90 {
         panic!(
             "Ai test failed with a score less then 90\n the ai score is: {}",
             avg
         )
     }
+}
+
+fn avg_br(teams: Vec<String>, br_data: &[(u16, f32)]) -> f32 {
+    avg(teams
+        .par_iter()
+        .map(|team| {
+            let (team, br) = br_data
+                .iter()
+                .find(|(team_num, _)| team == &format!("frc{}", team_num))
+                .unwrap();
+            println!("Team {} br is: {}", team, br);
+            br.round() as i16
+        })
+        .collect())
 }
